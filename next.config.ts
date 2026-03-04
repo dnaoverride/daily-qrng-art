6b7c718f72ec6241c9c54fe7c41386ed90316bf0
Marko Marinovic <markomarinovic@gmail.com>
2026-03-05 00:43:49 +0100

    404 webpack compatibility fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["dnaoverride.noip.me"],
+  output: "standalone",
+  async rewrites() {
+    return [
+      { source: "/next/:path*", destination: "/_next/:path*" },
+    ];
+  },
 };
 
 export default nextConfig;
